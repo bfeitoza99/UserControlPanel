a2de17427a8520fbe3011304cc5fd7754c33fed1
bfeitoza99 <bruno.feitoza10@hotmail.com>
2022-11-23 21:30:38 -0300

    adding git ignore rules

---
+++ backend/UserControlPanel/UserControlPanel.API/Controller/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserControlPanel.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+       
+    }
+}
