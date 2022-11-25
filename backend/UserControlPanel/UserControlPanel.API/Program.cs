b1c51abcdee252127a39fa65d2827f4a06c3004d
bfeitoza99 <bruno.feitoza10@hotmail.com>
2022-11-24 21:25:45 -0300

    adding dockerfile

--- backend/UserControlPanel/UserControlPanel.API/Program.cs
+++ backend/UserControlPanel/UserControlPanel.API/Program.cs
@@ -1,54 +1,26 @@
-using Microsoft.OpenApi.Models;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-builder.Services.AddSwaggerGen(c =>
+namespace UserControlPanel.API
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "UserControlPanel", Version = "v1", });
-});
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-builder.Services.AddEndpointsApiExplorer();
-
-
-
-builder.Services.AddControllers();
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+              .ConfigureLogging(logging =>
+              {
+                  logging.ClearProviders();
+                  logging.AddConsole();
+              })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseSwaggerUI(c =>
-{
-    c.RoutePrefix = string.Empty;
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-});
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
-app.UseCors(x => x
-              .AllowAnyMethod()
-              .AllowAnyHeader()
-              .SetIsOriginAllowed(origin => true)
-              .AllowCredentials());
-
-app.UseAuthorization();
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
-
-app.UseSwagger(x=> x.SerializeAsV2 = true);
-
-
-
-app.Run();
---
+++ backend/UserControlPanel/UserControlPanel.API/Startup.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace UserControlPanel.API
+{
+    public class Startup
+    {   
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddSwaggerGen();
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "UserControlPanel", Version = "v1", });
+            });
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddControllers();            
+        }
+
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //if (env.EnvironmentName)
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors(x => x
+                          .AllowAnyMethod()
+                          .AllowAnyHeader()
+                          .SetIsOriginAllowed(origin => true)
+                          .AllowCredentials());
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger(x => x.SerializeAsV2 = true);
+
+        }
+    }
+}
