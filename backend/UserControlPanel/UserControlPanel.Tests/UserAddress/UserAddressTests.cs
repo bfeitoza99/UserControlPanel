9c57b6eded60c78464334bad846b8c826f726c0e
bfeitoza99 <bruno.feitoza10@hotmail.com>
2022-11-28 00:16:43 -0300

    create unit tests

---
+++ backend/UserControlPanel/UserControlPanel.Tests/UserAddress/UserAddressTests.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserControlPanel.Application.Query.UserAdress;
+using UserControlPanel.Application.Query.UserGender;
+using UserControlPanel.Domain.Interfaces.Repository;
+using Xunit;
+
+namespace UserControlPanel.Tests.UserAddress
+{
+    public class UserAddressTests
+    {
+        [Fact]
+        public async Task UserAddress_GetByCep()
+        {
+
+            var mapper = new Mock<IMapper>();
+            var repository = new Mock<IUserGenderRepository>();
+
+            var handler = new Mock<UserAdressQueryHandle>(repository.Object, mapper.Object);
+
+            UserAdressQueryRequest query = new UserAdressQueryRequest("123445565");
+
+            var response = await handler.Object.Handle(query, default(CancellationToken));
+
+            Assert.IsType<UserAdressQueryResponse>(response);
+        }
+    }
+}
---
+++ backend/UserControlPanel/UserControlPanel.Tests/UserGender/UserGenderTests.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Moq;
+using UserControlPanel.Application.Query.UserGender;
+using UserControlPanel.Domain.Interfaces.Repository;
+using Xunit;
+
+namespace UserControlPanel.Tests.UserGender
+{
+    public class UserGenderTests
+    {
+        [Fact]
+        public async Task UserGender_GetAll()
+        {
+
+            var mapper = new Mock<IMapper>();
+            var repository = new Mock<IUserGenderRepository>();
+
+            var handler = new Mock<UserGenderQueryHandle>(repository.Object, mapper.Object);
+
+            UserGenderQueryRequest query = new UserGenderQueryRequest();
+
+            var response = await handler.Object.Handle(query, default(CancellationToken));
+
+            Assert.IsType<UserGenderQueryResponse>(response);
+        }
+    }
+}
